be0027becab41addc1b8f3cf033eeec0a01f4f5b
imdwpeng <imdwpeng@foxmail.com>
2026-03-14 20:51:01 +0800

    chore: 更新前端库内部资源索引

--- frontend/temp/declarations/jsb.d.ts
+++ frontend/temp/declarations/jsb.d.ts
@@ -1,1 +1,1 @@
-/// <reference path="C:\ProgramData\cocos\editors\Creator\3.8.6\resources\resources\3d\engine\@types\jsb.d.ts"/>
+/// <reference path="C:\ProgramData\cocos\editors\Creator\3.8.8\resources\resources\3d\engine\@types\jsb.d.ts"/>
